e6dd360c6b9fff784a3f42e6c806e713360e0929
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-06 21:56:31 +0530

    Time: 2 ms (69.24%) | Memory: 41.8 MB (23.73%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,19 +1,31 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int se = 0;
-        for(int i=0; i<s.length(); i++){
-            boolean status = false;
-            for(int j=se; j<=t.length()-1; j++){
-                if(s.charAt(i) == t.charAt(j)){
-                    se = j+1;
-                    status = true;
-                    break;
-                }
+        // int se = 0;
+        // for(int i=0; i<s.length(); i++){
+        //     boolean status = false;
+        //     for(int j=se; j<=t.length()-1; j++){
+        //         if(s.charAt(i) == t.charAt(j)){
+        //             se = j+1;
+        //             status = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!status){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int i = 0; // pointer for s
+        int j = 0; // pointer for t
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;  // move s pointer only on match
             }
-            if(!status){
-                return false;
-            }
+            j++;      // always move t pointer
         }
-        return true;
+
+        return i == s.length(); 
     }
 }
