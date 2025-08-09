3c2cd7e72b590574128beffcff92b9385684eab5
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-09 20:05:04 +0530

    Time: 2 ms (10.37%) | Memory: 41 MB (56.24%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,14 +1,49 @@
 class Solution {
     public int reverse(int x) {
-        long sum = 0;
-        while(x>0 || x<0){
-            int r = x%10;
-            sum = sum*10+r;
-            x = x/10;
+        // long sum = 0;
+        // while(x>0 || x<0){
+        //     int r = x%10;
+        //     sum = sum*10+r;
+        //     x = x/10;
+        // }
+        // if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+        //     return 0;
+        // }
+        // return (int)sum;
+
+            // String a = Math.abs(x) + "";
+            // long rev = 0;
+            // for (int i = a.length() - 1; i >= 0; i--) {
+            //     int ld = x % 10;
+            //     int num = (int) Math.pow(10, i);
+            //     rev += num * ld;
+            //     x /= 10;
+            // }
+            //  if ((rev <= Integer.MAX_VALUE) && (rev >= Integer.MIN_VALUE)){
+            //     return (int)rev;
+            //  }
+            // return 0;
+
+            String a = Math.abs((long)x) + ""; // use long to avoid overflow on abs
+        long rev = 0;
+
+        long temp = Math.abs((long)x); // work with positive number
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int ld = (int)(temp % 10); // extract last digit
+            long num = (long)Math.pow(10, i); // place value
+            rev += num * ld;
+            temp /= 10;
         }
-        if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
-            return 0;
+
+        // apply original sign
+        if (x < 0) {
+            rev = -rev;
         }
-        return (int)sum;
+
+        // check if within int range
+        if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
+            return (int)rev;
+        }
+        return 0;
     }
 }
