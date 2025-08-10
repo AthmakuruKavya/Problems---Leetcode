69adee0596f228639c7c90f89b129815342d6688
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-10 14:11:51 +0530

    Time: 1 ms (81.54%) | Memory: 41.4 MB (9.56%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,28 +1,17 @@
 class Solution {
     public int reverse(int x) {
-        // long sum = 0;
-        // while(x>0 || x<0){
-        //     int r = x%10;
-        //     sum = sum*10+r;
-        //     x = x/10;
-        // }
-        // if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
-        //     return 0;
-        // }
-        // return (int)sum;
+        long sum = 0;
+        while(x!=0){
+            int r = x%10;
+            sum = sum*10+r;
+            x = x/10;
+        }
+        if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return (int)sum;
 
-        // String a = Math.abs(x) + "";
-        // long rev = 0;
-        // for (int i = a.length() - 1; i >= 0; i--) {
-        //     int ld = x % 10;
-        //     int num = (int) Math.pow(10, i);
-        //     rev += num * ld;
-        //     x /= 10;
-        // }
-        //  if ((rev <= Integer.MAX_VALUE) && (rev >= Integer.MIN_VALUE)){
-        //     return (int)rev;
-        //  }
-        // return 0;
+        
 
         // String a = Math.abs(x) + ""; 
         // long rev = 0;
@@ -45,17 +34,17 @@ class Solution {
         // return 0;
 
 
-        long temp = (long)x;
-        String a = Math.abs(x) + "";
-        long rev = 0;
-        for (int i = a.length() - 1; i >= 0; i--) {
-            int num = (int) Math.pow(10, i);
-            rev += num * (temp % 10);
-            temp /= 10;
-        }
-        if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
-            return (int)rev;
-        }
-        return 0;
+        // long temp = (long)x;
+        // String a = Math.abs(x) + "";
+        // long rev = 0;
+        // for (int i = a.length() - 1; i >= 0; i--) {
+        //     int num = (int) Math.pow(10, i);
+        //     rev += num * (temp % 10);
+        //     temp /= 10;
+        // }
+        // if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
+        //     return (int)rev;
+        // }
+        // return 0;
     }
 }
