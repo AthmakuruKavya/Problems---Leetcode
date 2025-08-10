5f36039d871dff4f9fea5e8805e009ec97f05fcb
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-10 14:16:23 +0530

    Time: 2 ms (10.32%) | Memory: 40.7 MB (94.43%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,50 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        long sum = 0;
-        while(x!=0){
-            int r = x%10;
-            sum = sum*10+r;
-            x = x/10;
+        long temp = (long)x;
+        String a = Math.abs(x) + "";
+        long rev = 0;
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int num = (int) Math.pow(10, i);
+            rev += num * (temp % 10);
+            temp /= 10;
         }
-        if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+        if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
             return 0;
         }
-        return (int)sum;
-
-        
-
-        // String a = Math.abs(x) + ""; 
-        // long rev = 0;
-
-        // long temp = Math.abs(x);
-        // for (int i = a.length() - 1; i >= 0; i--) {
-        //     int ld = (int) (temp % 10);
-        //     long num = (long) Math.pow(10, i); 
-        //     rev += num * ld;
-        //     temp /= 10;
-        // }
-
-        // if (x < 0) {
-        //     rev = -rev;
-        // }
-
-        // if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
-        //     return (int) rev;
-        // }
-        // return 0;
-
-
-        // long temp = (long)x;
-        // String a = Math.abs(x) + "";
-        // long rev = 0;
-        // for (int i = a.length() - 1; i >= 0; i--) {
-        //     int num = (int) Math.pow(10, i);
-        //     rev += num * (temp % 10);
-        //     temp /= 10;
-        // }
-        // if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
-        //     return (int)rev;
-        // }
-        // return 0;
+       return (int)rev;
     }
 }
