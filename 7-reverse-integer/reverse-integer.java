d0167945be53622a0dcc267d82bab47b78ece315
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-10 14:02:49 +0530

    Time: 2 ms (10.32%) | Memory: 41 MB (56.11%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -24,23 +24,37 @@ class Solution {
         //  }
         // return 0;
 
-        String a = Math.abs(x) + ""; 
-        long rev = 0;
+        // String a = Math.abs(x) + ""; 
+        // long rev = 0;
 
-        long temp = Math.abs(x); // work with positive number
+        // long temp = Math.abs(x);
+        // for (int i = a.length() - 1; i >= 0; i--) {
+        //     int ld = (int) (temp % 10);
+        //     long num = (long) Math.pow(10, i); 
+        //     rev += num * ld;
+        //     temp /= 10;
+        // }
+
+        // if (x < 0) {
+        //     rev = -rev;
+        // }
+
+        // if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
+        //     return (int) rev;
+        // }
+        // return 0;
+
+
+        long temp = (long)x;
+        String a = Math.abs(x) + "";
+        long rev = 0;
         for (int i = a.length() - 1; i >= 0; i--) {
-            int ld = (int) (temp % 10);
-            long num = (long) Math.pow(10, i); 
-            rev += num * ld;
+            int num = (int) Math.pow(10, i);
+            rev += num * (temp % 10);
             temp /= 10;
         }
-
-        if (x < 0) {
-            rev = -rev;
-        }
-
         if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE) {
-            return (int) rev;
+            return (int)rev;
         }
         return 0;
     }
