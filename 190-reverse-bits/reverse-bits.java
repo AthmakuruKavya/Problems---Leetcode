241c02d58212bedd3ea95707a177268a02c88b9c
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-12 11:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (65.47%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,16 +1,26 @@
 class Solution {
     public int reverseBits(int n) {
-        String binaryNum = Integer.toBinaryString(n);
-        int bits = 32-binaryNum.length();
-        String bitsZ = "0".repeat(bits);
-        String binaryStr = bitsZ+binaryNum;
-        String rev = "";
-        for(int i = binaryStr.length()-1; i>=0; i--){
-            rev += binaryStr.charAt(i);
-        }
-        int num = Integer.parseInt(rev,2);
+        // String binaryNum = Integer.toBinaryString(n);
+        // int bits = 32-binaryNum.length();
+        // String bitsZ = "0".repeat(bits);
+        // String binaryStr = bitsZ+binaryNum;
+        // String rev = "";
+        // for(int i = binaryStr.length()-1; i>=0; i--){
+        //     rev += binaryStr.charAt(i);
+        // }
+        // int num = Integer.parseInt(rev,2);
 
-        return num;
+        // return num;
+
+        int reversed = 0;
+
+        for (int i = 0; i < 32; i++) {
+            reversed <<= 1;
+            reversed |= (n & 1);
+            n >>= 1;
+        }
+
+        return reversed;
 
     }
 }
