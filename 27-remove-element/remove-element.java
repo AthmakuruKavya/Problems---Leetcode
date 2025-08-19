e6786875cad2ef8bf9b096ad09c5eb7a54737563
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-19 15:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (83.98%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int si = 0;
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                swap(si,i,nums);
+                count++;
+                si++;
+            }
+        }
+        return count++;
+    }
+    static void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
