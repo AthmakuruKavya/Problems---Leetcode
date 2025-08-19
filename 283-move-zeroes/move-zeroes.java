c700f581b0cb5789c8ba58ea4b2cd1c5376ff4fc
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-19 17:02:29 +0530

    Time: 2 ms (79.90%) | Memory: 46.5 MB (24.80%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,47 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] != 0) {
-                        swap(i, j, nums);
-                        break;
-                    }
-                }
-            }
-        }
+    void swap(int[] arr,int n1,int n2){
+      int temp = arr[n2];
+      arr[n2] = arr[n1];
+      arr[n1] = temp;
+    
+
     }
 
-    static void swap(int i, int j, int[] nums) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void moveZeroes(int[] nums) {
+        int n1 =0;
+        int n2 =0;
+
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[n2]!=0){
+                swap(nums,n1,n2);
+                n1++;
+            }
+            n2++;
+        }
+        
     }
 }
+
+
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == 0) {
+//                 for (int j = i + 1; j < nums.length; j++) {
+//                     if (nums[j] != 0) {
+//                         swap(i, j, nums);
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     static void swap(int i, int j, int[] nums) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
