b746401636766f84b461986dc26e955a403153b3
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-08-19 15:01:05 +0530

    Time: 1 ms (77.42%) | Memory: 45.8 MB (58.87%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,36 @@
 class Solution {
-    public int maxProfit(int[] nums) {
-        int small = Integer.MAX_VALUE;
-        int big = 0;
-        int dif = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < small) {
-                small = nums[i];
-                big = nums[i];
+    public int maxProfit(int[] stocks) {
+        int profit = 0;
+        for(int i=0; i<stocks.length-1; i++){
+            int val = stocks[i+1]-stocks[i];
+            if(val>0){
+                profit = val+profit;
             }
-            if (nums[i] > big) {
-                big = nums[i];
-                int val = big - small + dif;
-                if (val > dif) {
-                    dif = val;
-                }
-                small = nums[i];
-            }
-
         }
-        return dif;
+        return profit;
+
+
+
+
+
+        // int small = Integer.MAX_VALUE;
+        // int big = 0;
+        // int dif = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] < small) {
+        //         small = nums[i];
+        //         big = nums[i];
+        //     }
+        //     if (nums[i] > big) {
+        //         big = nums[i];
+        //         int val = big - small + dif;
+        //         if (val > dif) {
+        //             dif = val;
+        //         }
+        //         small = nums[i];
+        //     }
+
+        // }
+        // return dif;
     }
 }
